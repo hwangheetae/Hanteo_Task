8fd727390d940e010832916e71360bc6fa85d877
heetaehwang <tejaia@naver.com>
2025-03-30 15:44:24 +0900

    refactor: Footer 컴포넌트 분리

---
+++ src/component/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      <p className="text-sm">Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./component/Header";
+import Footer from "./component/Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,10 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="min-h-screen flex flex-col mx-auto border border-gray-300 shadow-lg max-w-[425px]">
       <Header />
       <main className="flex-1 overflow-y-auto  bg-gray-100">{children}</main>
-
-      <footer className="bg-gray-800 text-white text-center py-4">
-        <p className="text-sm">Footer</p>
-      </footer>
+      <Footer />
     </div>
   );
 };
